6fb81e250c92cba33d5df07c8ab9f0bf1df2d69e
Vitaliy Biryuckov <stukov.biryuckov2017@yandex.ru>
2022-03-21 21:48:24 +0800

    Init

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+var lang = 'eng';
+text = {
+    eng: {
+        'header': 'Unstable Problems of Computaional Mathematics'
+    },
+
+    ru: {
+        'header': 'Неустойчивые Задачи Вычислительной Математики'
+    }
+}
+
+
+var headerText = document.querySelector('.header-text-container');
+
+function changeLanguage(lang='eng') {
+    headerText.innerHTML += 'afsaf'
+    return headerText;
+};
